ebc3cd2f56f6810db2540200500580e6652dc8a7
alaaaliinnovitics-ctrl <alaa.ali.innovitics@gmail.com>
2025-11-25 10:42:40 +0200

    Update WaAPIServiceProvider.php

--- src/WaAPIServiceProvider.php
+++ src/WaAPIServiceProvider.php
@@ -1,30 +1,4 @@
-// <?php
 
-// namespace WaAPI\WaAPI;
-
-// use Spatie\LaravelPackageTools\Package;
-// use Spatie\LaravelPackageTools\PackageServiceProvider;
-
-// class WaAPIServiceProvider extends PackageServiceProvider
-// {
-//     public function configurePackage(Package $package): void
-//     {
-//         /*
-//          * This class is a Package Service Provider
-//          *
-//          * More info: https://github.com/spatie/laravel-package-tools
-//          */
-//         $package
-//             ->name('laravel-waapi')
-//           //  ->hasRoute('api')
-//             ->hasConfigFile('waapi');
-//     }
-//     public function boot()
-// {
-//     $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
-// }
-
-// }
 <?php
 
 namespace WaAPI\WaAPI;
